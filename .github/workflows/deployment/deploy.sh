302f897eb0cc9cd55248ea6921edf9c1be642696
visortelle <visortelle@gmail.com>
2021-12-30 11:12:53 +0100

    Handle branch deletion

--- .github/workflows/deployment/deploy.sh
+++ .github/workflows/deployment/deploy.sh
@@ -7,12 +7,6 @@ set -eo pipefail
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $script_dir/commons.sh
 
-# Monkey patch html files by adding <meta name="robots" content="noindex, nofollow">.
-# It will tell search bots like googlebot not to index this content.
-search_robots_noindex() {
-  # find
-}
-
 deploy() {
   if [[ ! -z "$GITHUB_REF_NAME" ]]; then
     # The GITHUB_REF_NAME env variable is available in github actions.
@@ -39,6 +33,10 @@ deploy() {
     # Restore temporary backup for other branches content.
     mv ./branches "${gh_pages_dir}/"
   else
+    # Patch html files to tell search engine bots like google-bot not to index this content.
+    # More info: https://developers.google.com/search/docs/advanced/crawling/block-indexing
+    find $site_src -name '*.html' -exec sed -i 's/<head>/<head><meta name="robots" content="noindex, nofollow">/g' {}
+
     branch_slug=$(slugify $git_branch)
     site_dest="${gh_pages_dir}/branches/${branch_slug}"
 
